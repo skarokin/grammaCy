a8b1e0659f7c65a0e3f85506f0ed3e684c9d6beb
skarokin <sean.kim0327@gmail.com>
2024-03-20 23:46:00 -0400

    extract noun chunk + associated verb phrase

--- src/grammar/subject_verb_agreement.py
+++ src/grammar/subject_verb_agreement.py
@@ -13,3 +13,49 @@
 # 11. collective nouns are singular
 # extra rules:
 # - cannot add numbers to noncount nouns
+
+import spacy
+from spacy import displacy
+
+# temporary until our own model is trained
+nlp = spacy.load('en_core_web_sm')
+
+# a verb phrase is simply [helping verb(s) + main verb + prepositional phrase(s)] so we can use two pointer expansion
+# remember that adverb must come before the verb, and preposition must come after
+def extract_verb_phrases(token, doc):
+    verb_phrases = []
+    left = token.i
+    right = token.i
+    while left > 0 and doc[left-1].pos_ in ['AUX', 'ADV']:
+        left -= 1
+    while right < len(doc)-1 and doc[right+1].pos_ in ['AUX', 'ADP']:
+        right += 1
+    verb_phrases.append(doc[left:right+1])
+    return verb_phrases
+
+def extract_noun_chunks(doc):
+    return list(doc.noun_chunks)
+
+def subject_verb_relationship(doc):
+    # the i-th noun chunk is the subject of the i-th verb phrase
+    noun_chunks = extract_noun_chunks(doc)
+    verb_phrases = []
+
+    for nc in noun_chunks:
+        if nc.root.head.pos_ == 'VERB' or nc.root.head.pos_ == 'AUX':
+            verb_phrases.extend(extract_verb_phrases(nc.root.head, doc))
+    
+    return noun_chunks[:len(verb_phrases)], verb_phrases
+
+sentences = ['The dog quickly ran to the park.',
+             'She is sleeping in the house.',
+             'I just went to the store to buy a new computer.',
+             'My mousepad is a little bit dirty.',
+             'He has been working hard, but is still not making enough money.']
+
+for s in sentences:
+    doc = nlp(s)
+    noun_chunks, verb_phrases = subject_verb_relationship(doc)
+    print(noun_chunks, verb_phrases)
+
+displacy.serve(list(nlp(s) for s in sentences), style='dep')
