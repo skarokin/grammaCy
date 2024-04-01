6c5e06c0e95a76a6a8dbf0d4f8d4078c00cb8dad
Pranshu Sarin <sarinpranshu@gmail.com>
2024-04-01 15:37:08 -0400

    Updated to use the tree

--- src/grammar/conjunctions.py
+++ src/grammar/conjunctions.py
@@ -5,35 +5,31 @@ from spacy import displacy
 import re
 
 # temporary until our own model is trained
-
+nlp = spacy.load('en_core_web_sm')
 def duplicateAnds (sentences):
 
     numOfDuplicates = 0
     for sentence in sentences:
-        sentence = sentence.lower()
-        tokenizedSentence = re.split(r'\s+', sentence)
+        tokenizedSentence = nlp(sentence)
         foundAnd = False; 
         for tokenizedWord in tokenizedSentence:
-            if tokenizedWord == "And": 
-                print(tokenizedWord == "and")
-            if tokenizedWord.casefold() == "and":
+            if str(tokenizedWord) == "and":
                 if foundAnd == False:
                     foundAnd = True
                 elif foundAnd == True: 
                     numOfDuplicates += 1
     return numOfDuplicates
 
-
-
 def main(sentences):
+    
     res = duplicateAnds(sentences)
     if res>0:
         print ("Minor Style Error: Detected two or more \"and\" in the sentence")
 
     
 
-sentences = ['I like Chelsea and Juventus and hate Tottenham and AC Milan'
-              'I went to the Andes and Lake Titicaca'
+sentences = ['I like Chelsea and Juventus and hate Tottenham and AC Milan',
+              'I went to the Andes and Lake Titicaca',
               'And then I walked and then I slept']
 main(sentences)
 
