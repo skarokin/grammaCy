b3dbe2bb5a11b705d9bf0a80090e0cb8f9620c00
skarokin <sean.kim0327@gmail.com>
2024-05-13 19:38:08 -0400

    continuing augmentor work

--- src/conllu_augmentor.py
+++ src/conllu_augmentor.py
@@ -19,22 +19,71 @@
 from word_forms.word_forms import get_word_forms
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
+class ConlluAugmentor:
+    '''A class to augment a dataset of .conllu files by injecting errors into sentences of interest'''
 
-# note: not perfect; sometimes generates extra forms that do not exist
-# extract first form that matches desired POS 
-def get_forms(word, pos):
-    forms = get_word_forms(word)
-    for p in forms:
-        for w in forms[p]:
-            curr = nlp(w)[0]
-            if curr.tag_ == pos:
-                return (curr.text, curr.tag_)
+    nlp = spacy.load('en_core_web_sm')    # since we are only using POS tagger, we can use small model
 
-# for given .conllu file, find a specific dependency relation and specific POS tag to augment and whether to use head or child
-# create a new .conllu file with the augmented data and append the augmented data to original .conllu file (make sure we update sentence id)
-def augment_conllu(conllu_path, dep_rel, pos, child=False):
-    pass
+    # data_dir: directory containing .conllu files
+    # rules: list of rule tuples [(dep_rel, pos, aug_pos, child=False, probability=0.5)] 
+    def __init__(self, data_dir, rules=None):
+        self.data_dir = data_dir
+        self.rules = rules
 
-print(get_forms('run', 'VBG'))
-print(get_forms('amazing', 'RB'))
+    # note: not perfect; sometimes generates extra forms that do not exist
+    # extract first form that matches desired POS using spacy's POS tagger
+    @staticmethod
+    def get_forms(word, pos):
+        forms = get_word_forms(word)
+        for p in forms:
+            for w in forms[p]:
+                curr = ConlluAugmentor.nlp(w)[0]
+                if curr.tag_ == pos:
+                    return (curr.text, curr.tag_)
+            
+    # format string of conllu data into a list of sentences, where each sentence is a list of words with all features
+    @staticmethod
+    def format_data(data):
+        data = data.split('# sent_id =')[1:]
+        splitted_data = ['' for _ in range(len(data))]
+        for item in data:
+            item_split = item.split('\n', 1)
+            sent_id = int(item_split[0])
+            lines = item_split[1].split('\n')
+            split_lines = [line.split('\t') for line in lines if line.strip()]  # split by tab, ignore extraneous lines
+            splitted_data[sent_id] = split_lines
+        return splitted_data
+    
+    # look for specific relation between two words and inject desired error
+    # [(dep_rel, pos, aug_pos, child=False, probability=0.5), ...]
+    # dep_rel: dependency relation to look for
+    # pos: POS tag to look for in the dependency relation
+    # aug_pos: POS tag to change to
+    # child: whether to augment child or head of dependency relation
+    # probability: probability of augmenting sentence if it matches dep_rel and pos
+    def add_rules(self, rule):
+        # if dep_rel, pos, aug_pos already exists, this is a duplicate rule
+        if (rule[0], rule[1], rule[2]) in self.rules:
+            return
+
+        self.rules.append(rule)
+            
+    # open .conllu file and return formatted data
+    def open_conllu_file(self):
+        if not self.conllu_path.endswith('.conllu'):
+            return None
+        
+        with open(self.conllu_path, 'r') as f:
+            data = f.read()
+        return self.format_data(data)
+
+    # for given .conllu file, find a specific dependency relation and specific POS tag to augment and whether to use head or child
+    # create a new .conllu file with the augmented data and append the augmented data to original .conllu file (make sure we update sentence id)
+    # augments on a file-by-file basis, so we can run this on a deep traversal of the data directory
+    def augment_conllu_file(self, conllu_path, rules=None,):
+        if rules is None:
+            raise ValueError('No rules specified for augmentation, aborting...')
+
+    # augment entire dataset by traversing data directory and augmenting each .conllu file according to rules in add_rules
+    def augment_dataset(self):
+        pass
