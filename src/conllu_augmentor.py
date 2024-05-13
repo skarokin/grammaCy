6563e28a1c77a952fc0eda4a4d0b2a268d13cf06
skarokin <sean.kim0327@gmail.com>
2024-05-13 01:58:34 -0400

    identified more grammar tings to augment and some to not

--- src/conllu_augmentor.py
+++ src/conllu_augmentor.py
@@ -2,10 +2,20 @@
 # the error injection is only changing the POS tags of the words (and also obviously the word itself) because we want
 # to ensure that the dependency parser can recognize the correct dependencies even if the POS tags are wrong
 # ERRORS WE NEED TO SIMULATE:
-#   - preposition + verb: for each preposition, take its 'prep' dependency and change the verb to base form
-#       - this is needed because the current parser would see prep + base form verb as prep + noun
-#       - or, it would assign the wrong POS to the preposition (like sconj or particle)
-#   - for other confirmed augmentations we will figure this out later (check the pins in discord groupchat)
+#   - noun + gerund: if gerund is 'nsubj' of noun, change gerund to base form verb
+#       - in auxiliary + base form verb, model sometimes thinks the base form verb is a noun
+#   - adp + gerund: if gerund is 'pcomp' of preposition, change gerund to base form verb 
+#       - in adp + base form verb, model thinks the base form verb is a noun
+#   - adjective: for each adjective, change to adverb (and vice versa)
+#       - model sometimes thinks misplaced adjectives are adverbs and vice versa
+#       - suggestion: if adjective is 'advmod' of verb, suggest change to adverb (bc this is incorrect placement of adjective) and etc
+# ERRORS WE DON'T NEED TO SIMULATE:
+#   - subject verb agreement: model already diffentiates between singular and plural nouns even if ungrammatical sentence
+#       - suggest a different form of the verb before the noun
+#   - auxiliary verbs: model already can handle wrong form of aux verbs like 'be' and 'have'
+#       - suggest a different form of auxiliary verb based on context
+#   - gerund vs past tense: model already can differentiate between gerund and past tense verbs
+#       - suggest adding a preposition before the gerund OR making the gerund a past tense verb
 from word_forms.word_forms import get_word_forms
 import spacy
 
